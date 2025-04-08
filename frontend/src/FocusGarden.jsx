8702a1c7eeece40edbc51f565f60e60b8d9c7def
hgomeUIC <hgome@uic.edu>
2025-04-07 20:12:58 -0500

    Added new changes to fronend UI. Added the garden image and a template to change each cell of the grid for a plant

--- frontend/src/FocusGarden.jsx
+++ frontend/src/FocusGarden.jsx
@@ -3,9 +3,11 @@ import { useNavigate } from 'react-router-dom';
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "./firebase";
 import './FocusGarden.css'
+import ImageGrid from './imageGrid.jsx'
 
 function FocusGarden() {
 	const navigate = useNavigate();
+	const farmGrid = 'Farmland.png'
 	const FocusGardenPath = () => {
 		navigate('/'); 
 	};
@@ -14,6 +16,9 @@ function FocusGarden() {
 		<div className="FGDiv">
 			<h1 className = "gardenTitle">Focus Garden </h1>
 			<button onClick={FocusGardenPath}className="buttonFG">LeaderBoard</button>
+			<div className = "GridLocation">
+				<ImageGrid/>
+			</div>
 		</div>
     </div>
   );
